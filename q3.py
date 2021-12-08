5444edc6219a280d973936e98855f08288a133fc
Dhruv Meduri <mdhruv@iisc.ac.in>
2021-12-08 17:40:38 +0530

    First commit

---
+++ q3.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
+# First we uniformly generate points in the given ellipse
+a = 10 #Major axis
+b = 5 #Minor axis
+weight1 = np.random.uniform(0,1)
+weight2 = np.random.uniform(0,1)
+weights = np.array([weight1,weight2])# Weight matrix
+learn = 10**(-5)
+#lst = []
+for i in range(1000000):
+    print(i)
+    x0 = np.random.uniform(-a/2,a/2)
+    y0 = np.random.uniform((-b/(2*a))*(np.sqrt((a*a)-(4*x0*x0))),(b/(2*a))*(np.sqrt((a*a)-(4*x0*x0))))
+    x = (x0 - y0)/np.sqrt(2)
+    y = (x0 + y0)/np.sqrt(2)
+    input = np.array([x,y])
+    output = np.dot(input,weights)
+    weights = weights + learn*output*(input - output*weights)
+    #plt.scatter(x,y,color='blue')
+#plt.show()
+print('Eigenvector:',weights)
+#To compute the Eigenvalue
+
+sum = 0
+for j in range(1000000):
+    x0 = np.random.uniform(-a/2,a/2)
+    y0 = np.random.uniform((-b/(2*a))*(np.sqrt((a*a)-(4*x0*x0))),(b/(2*a))*(np.sqrt((a*a)-(4*x0*x0))))
+    x = (x0 - y0)/np.sqrt(2)
+    y = (x0 + y0)/np.sqrt(2)
+    input = np.array([x,y])
+    output = np.dot(input,weights)
+    #lst.append(output)
+    sum = sum + (output*output)
+var = sum/(1000000-1)
+
+print('Eigenvalue:',var)
